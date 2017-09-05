54aba465b56d4e0931b521cc2682e31ce72cca75
Alex Prokudin <prokudin89@gmail.com>
2017-09-05 21:40:44 +0700

    review server side

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,21 +1,78 @@
 'use strict';
 
-var percolator = require('percolator').Percolator;
+const http = require('http');
+const express = require('express');
+const ws = require('ws');
+let geoip = require('geoip-lite');
+let useragent = require('useragent');
 
-var Server = function(port) {
-    var server = new percolator({
-        'port': port,
-        'autoLink': false
-    });
-
-    server.route('/api/about', {
-        GET: function(req, res) {
-            res.object({
-                'status': 'ok'
-            }).send();
-        }
-    });
-    return server;
+const config = {
+    port: 8080,
+    wshost: 'ws://localhost:8080'
 };
 
-module.exports.Server = Server;
+const app = express();
+const server = http.Server(app);
+const wss = new ws.Server({
+    server,
+    path: '/',
+    clientTracking: false,
+    maxPayload: 1024
+});
+
+app.disable('x-powered-by');
+server.listen(config.port);
+
+const users = {};
+let userCount = 0;
+let userLastId = 0;
+
+setInterval(() => console.log(`Users online: ${userCount}`), 10 * 1000);
+
+wss.on('connection', (socket, message) => {
+    userCount++;
+    const id = userLastId++;
+    const ip = message.headers['x-real-ip'] || message.connection.remoteAddress;
+    const user = {
+        id,
+        host: message.headers.host,
+        ip,
+        ipgeo: geoip.lookup(ip),
+        ua: useragent.lookup(message.headers['user-agent']).toJSON(),
+        date: Date.now(),
+        updated: Date.now()
+    };
+
+    users.id = user;
+
+    socket.once('close', () => {
+        delete users[id];
+        userCount--;
+    });
+});
+
+wss.on('error', (err) => {
+    console.error(err);
+});
+
+app.get('/analytics.js', (req, res) => {
+    const js = `var socket = new WebSocket('${config.wshost}');`;
+    res.set('Content-Type', 'application/javascript');
+    res.send(js);
+});
+
+app.get('/test/*', (req, res) => {
+    const html = `
+        <!doctype html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <title>Test Page</title>
+            </head>
+            <body>
+                <h1>test page</h1>
+                <script src="/analytics.js"></script>
+            </body>
+            </html>`;
+    res.send(html);
+});
